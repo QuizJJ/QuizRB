6ed8da6ac52a10b0b2e1119351927805c339824c
QuizJJ <projetotiif@gmail.com>
2021-02-08 20:06:22 -0300

    Commit 3

---
+++ app/src/main/java/com/example/quizr/HomeUserActivity.java
@@ -0,0 +1,16 @@
+package com.example.quizr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_user);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/quizr/OpcoesActivity.java
@@ -0,0 +1,14 @@
+package com.example.quizr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OpcoesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opcoes);
+    }
+}
